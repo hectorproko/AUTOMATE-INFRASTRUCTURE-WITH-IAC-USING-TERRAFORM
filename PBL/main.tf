2477a95b8b1e5e8bb8e011349e2f701cd6ed04a8
Hector A. Rodriguez <hectiquin2003@hotmail.com>
2022-05-11 20:34:07 -0400

    vars

--- PBL/main.tf
+++ PBL/main.tf
@@ -1,26 +1,42 @@
-provider "aws" {
-  region = "us-east-1"
+variable "region" {
+    default = "us-east-1"
 }
-# Create VPC
-resource "aws_vpc" "main" {
-  cidr_block                     = "172.16.0.0/16"
-  enable_dns_support             = "true"
-  enable_dns_hostnames           = "true"
-  enable_classiclink             = "false"
-  enable_classiclink_dns_support = "false"
+provider "aws" {
+    region = var.region
 }
 
-# Create public subnets1
-    resource "aws_subnet" "public1" {
-    vpc_id                     = aws_vpc.main.id
-    cidr_block                 = "172.16.0.0/24"
-    map_public_ip_on_launch    = true
-    availability_zone          = "us-east-1a"
+variable "vpc_cidr" {
+    default = "172.16.0.0/16"
 }
-# Create public subnet2
-    resource "aws_subnet" "public2" {
-    vpc_id                     = aws_vpc.main.id
-    cidr_block                 = "172.16.1.0/24"
-    map_public_ip_on_launch    = true
-    availability_zone          = "us-east-1b"
+
+variable "enable_dns_support" {
+    default = "true"
 }
+
+variable "enable_dns_hostnames" {
+    default ="true" 
+}
+
+variable "enable_classiclink" {
+    default = "false"
+}
+
+variable "enable_classiclink_dns_support" {
+    default = "false"
+}
+
+# Create VPC
+resource "aws_vpc" "main" {
+  cidr_block                     = var.vpc_cidr
+  enable_dns_support             = var.enable_dns_support
+  enable_dns_hostnames           = var.enable_dns_hostnames
+  enable_classiclink             = var.enable_classiclink
+  enable_classiclink_dns_support = var.enable_classiclink_dns_support
+}
+
+
+# Get list of availability zones
+data "aws_availability_zones" "available" {
+state = "available"
+}
+
