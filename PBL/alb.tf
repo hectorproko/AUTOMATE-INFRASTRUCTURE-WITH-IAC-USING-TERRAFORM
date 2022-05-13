912d62ff824eb5b9e6efb29630bc8c88cac67e01
Hector A. Rodriguez <hectiquin2003@hotmail.com>
2022-05-13 16:05:32 -0400

    ext-alb

---
+++ PBL/alb.tf
@@ -0,0 +1,46 @@
+resource "aws_lb" "ext-alb" {
+    name     = "ext-alb"
+    internal = false
+    security_groups = [
+    aws_security_group.ext-alb-sg.id,
+    ]
+    subnets = [
+    aws_subnet.public[0].id,
+    aws_subnet.public[1].id
+    ]
+    tags = merge(
+        var.tags,
+        {
+            Name = "ACS-ext-alb"
+        },
+    )
+    ip_address_type    = "ipv4"
+    load_balancer_type = "application"
+}
+
+resource "aws_lb_target_group" "nginx-tgt" {
+    health_check {
+        interval            = 10
+        path                = "/healthstatus"
+        protocol            = "HTTPS"
+        timeout             = 5
+        healthy_threshold   = 5
+        unhealthy_threshold = 2
+    }
+    name        = "nginx-tgt"
+    port        = 443
+    protocol    = "HTTPS"
+    target_type = "instance"
+    vpc_id      = aws_vpc.main.id
+}
+
+resource "aws_lb_listener" "nginx-listner" {
+    load_balancer_arn = aws_lb.ext-alb.arn
+    port              = 443
+    protocol          = "HTTPS"
+    certificate_arn   = aws_acm_certificate_validation.hracompany.certificate_arn
+    default_action {
+        type             = "forward"
+        target_group_arn = aws_lb_target_group.nginx-tgt.arn
+    }
+}
