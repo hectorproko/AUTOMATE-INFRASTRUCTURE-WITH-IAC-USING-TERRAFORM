e5300c2e99d81fa77583f5768194dd5ad7d949c9
Hector A. Rodriguez <hectiquin2003@hotmail.com>
2022-05-13 12:56:22 -0400

    webserver secuirty group

--- PBL/security.tf
+++ PBL/security.tf
@@ -123,3 +123,45 @@ resource "aws_security_group_rule" "inbound-ialb-https" {
     source_security_group_id = aws_security_group.nginx-sg.id
     security_group_id        = aws_security_group.int-alb-sg.id
 }
+
+# security group for webservers, to have access only from the internal load balancer and bastion instance
+resource "aws_security_group" "webserver-sg" {
+  name   = format("%s-webserver", var.name)
+  vpc_id = aws_vpc.main.id
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    tags = merge(
+        var.tags,
+        {
+        Name = format("%s-webserver", var.name)
+        },
+    )
+}
+resource "aws_security_group_rule" "inbound-web-http" {
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.int-alb-sg.id
+  security_group_id        = aws_security_group.webserver-sg.id
+}
+resource "aws_security_group_rule" "inbound-web-https" {
+  type                     = "ingress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.int-alb-sg.id
+  security_group_id        = aws_security_group.webserver-sg.id
+}
+resource "aws_security_group_rule" "inbound-web-ssh" {
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.bastion_sg.id
+  security_group_id        = aws_security_group.webserver-sg.id
+}
