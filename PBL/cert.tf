590b7ba462c3b0bcea3b9faece7a9bd35f4c3bb5
Hector A. Rodriguez <hectiquin2003@hotmail.com>
2022-05-13 15:04:52 -0400

    validation method

--- PBL/cert.tf
+++ PBL/cert.tf
@@ -8,8 +8,25 @@ resource "aws_acm_certificate" "hracompany" {
 resource "aws_route53_zone" "hracompany" {
   name = "hracompany.ga"
 }
-# calling the hosted zone
-data "aws_route53_zone" "hracompany" {
-  name         = "hracompany.ga"
-  private_zone = false
+# # calling the hosted zone
+# data "aws_route53_zone" "hracompany" {
+#   name         = "hracompany.ga"
+#   private_zone = false
+# }
+
+# selecting validation method
+resource "aws_route53_record" "hracompany" {
+  for_each = {
+    for dvo in aws_acm_certificate.hracompany.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.hracompany.zone_id
 }
