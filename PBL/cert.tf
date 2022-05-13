795d9f4a42871366ae053254f24582663ce713e2
Hector A. Rodriguez <hectiquin2003@hotmail.com>
2022-05-13 14:26:06 -0400

    acm

--- PBL/cert.tf
+++ PBL/cert.tf
@@ -1,3 +1,10 @@
+# The entire section create a certiface, public zone, and validate the certificate using DNS method
+# Create the certificate using a wildcard for all the domains created in hracompany.ga
+resource "aws_acm_certificate" "hracompany" {
+  domain_name       = "*.hracompany.ga"
+  validation_method = "DNS"
+}
+
 # calling the hosted zone
 data "aws_route53_zone" "hracompany" {
   name         = "hracompany.ga"
