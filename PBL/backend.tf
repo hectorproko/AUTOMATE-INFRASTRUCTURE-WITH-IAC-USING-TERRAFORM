fd762faa8c65aef6b4c6e8345a3444a7819fd29e
Hector A. Rodriguez <hectiquin2003@hotmail.com>
2022-05-16 17:47:37 -0400

    m

--- PBL/backend.tf
+++ PBL/backend.tf
@@ -29,8 +29,6 @@ resource "aws_dynamodb_table" "terraform_locks" {
 #Terraform expects that both S3 bucket and DynamoDB resources are already created before we configure the backend. So, let us run terraform apply to provision resources.
 # #Configure S3 Backend
 terraform {
-    depends_on    = [aws_s3_bucket.terraform_state]
-
   backend "s3" {
     bucket         = "hector-dev-terraform-bucket"
     key            = "global/s3/terraform.tfstate"
