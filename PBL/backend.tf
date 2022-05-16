9ffeaa745799fc7c7e867a0dd0dac00919594512
Hector A. Rodriguez <hectiquin2003@hotmail.com>
2022-05-16 12:50:04 -0400

    fixed region

--- PBL/backend.tf
+++ PBL/backend.tf
@@ -32,7 +32,7 @@ terraform {
   backend "s3" {
     bucket         = "hector-dev-terraform-bucket"
     key            = "global/s3/terraform.tfstate"
-    region         = "eu-central-1"
+    region         = "us-east-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
