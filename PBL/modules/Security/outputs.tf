db8c7bb067ccee86a70d0d65c0cac5846f29752b
Hector A. Rodriguez <hectiquin2003@hotmail.com>
2022-05-19 12:02:15 -0400

    fix

--- PBL/modules/Security/outputs.tf
+++ PBL/modules/Security/outputs.tf
@@ -5,25 +5,25 @@ output "ALB-sg" {
 
 
 output "IALB-sg" {
-  value = aws_security_group.HRA[format("%s-int-ALB", var.name)].id
+  value = aws_security_group.HRA[format("int-alb-sg")].id
 }
 
 
 output "bastion-sg" {
-  value = aws_security_group.HRA[format("%s-bastion", var.name)].id
+  value = aws_security_group.HRA[format("bastion-sg")].id
 }
 
 
 output "nginx-sg" {
-  value = aws_security_group.HRA[format("%s-nginx-reverse-proxy", var.name)].id
+  value = aws_security_group.HRA[format("nginx-sg", var.name)].id
 }
 
 
 output "web-sg" {
-  value = aws_security_group.HRA[format("%s-webserver", var.name)].id
+  value = aws_security_group.HRA[format("webserver-sg")].id
 }
 
 
 output "datalayer-sg" {
-  value = aws_security_group.HRA[format("%s-datalayer", var.name)].id
+  value = aws_security_group.HRA[format("datalayer-sg")].id
 }
