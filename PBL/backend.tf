af5fdea78baa89773519c4a0334df3256db433b1
Hector A. Rodriguez <hectiquin2003@hotmail.com>
2022-05-18 21:57:57 -0400

    Project 18

--- PBL/backend.tf
+++ PBL/backend.tf
@@ -1,42 +1,5 @@
-#1 Make sure bucket exist with same name
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "hector-dev-terraform-bucket"
-  # Enable versioning so we can see the full revision history of our state files
-  versioning {
-    enabled = true
-  }
-  # Enable server-side encryption by default
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-  tags = merge(
-    var.tags,
-    {
-      Name = format("%s-terraform-state", var.name)
-    },
-  )
-}
 
-#2 Dynamo DB resource for locking and consistency checking:
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-  tags = merge(
-    var.tags,
-    {
-      Name = format("%s-terraform-locks", var.name)
-    },
-  )
-}
+
 
 #Terraform expects that both S3 bucket and DynamoDB resources are already created before we configure the backend. So, let us run terraform apply to provision resources.
 # # #Configure S3 Backend
@@ -49,4 +12,3 @@ terraform {
     encrypt        = true
   }
 }
-
